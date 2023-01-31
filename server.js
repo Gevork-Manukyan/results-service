34287cdffbafc83a94b4747f9e6b77639c42af06
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-01-30 16:46:41 -0800

    sets up basic middleware

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const morgan = require("morgan")
+
+
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json()) // Parse incoming requests with JSON payloads
+app.use(morgan("tiny")) // Log requests info
+
+
 
 app.get("/test", (req, res) => {
     console.log(req.body)
