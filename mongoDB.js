da5b46e7ebaa3cba8b13ae3778f3584191360f34
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2023-02-02 10:04:28 -0800

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -1,5 +1,4 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://gmanukyan:grMgan46525@results-service.n4rovio.mongodb.net/?retryWrites=true&w=majority";
 const uri = "mongodb://localhost:27017/test";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
