b673ec9319f78ffcef49b6c0fa4dec4ee9f39ce5
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-06 23:07:31 -0800

    installs mongoose and updates db.js

--- Server/database.js
+++ Server/database.js
@@ -1,24 +1,22 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb://localhost:27017/test";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const mongoose = require("mongoose")
+const mongoURI = process.env.MONGODB_URI
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.set('strictQuery', false);
+mongoose.connect(
+    mongoURI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("MongoDB Connected Successfully");
 });
 
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const resultSchema = new Schema({
-  response: {type: String, required: true},
-  size: {type: String, required: true},
-  relation: {type: String, required: true},
-  region: {type: String, required: true},
-  confidence: {type: Number, required: true}
-})
-
-const Result = mongoose.model('Result', resultSchema)
-module.exports = Result
+module.exports = {
+    mongoose,
+    db
+}
